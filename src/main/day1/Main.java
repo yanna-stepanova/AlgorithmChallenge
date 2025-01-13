23468233df432edbe1397d4a8685362ad31932eb
Yana Stepanova <yanna.stepanova@gmail.com>
2025-01-13 14:28:16 +0200

    day 1

---
+++ src/main/day1/Main.java
@@ -0,0 +1,11 @@
+package main.day1;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<Integer> result = solution.stableMountains(new int[]{1, 2, 3, 4, 5}, 2);
+        result.forEach(System.out::println);
+    }
+}
