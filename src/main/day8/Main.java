0644f6a610192360fa1e3a2edb2e096c778933da
Yana Stepanova <yanna.stepanova@gmail.com>
2025-01-22 16:08:26 +0200

    day8

---
+++ src/main/day8/Main.java
@@ -0,0 +1,8 @@
+package main.day8;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.convertDateToBinary("2080-02-29"));
+    }
+}
---
+++ src/main/day8/Solution.java
@@ -0,0 +1,12 @@
+package main.day8;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String convertDateToBinary(String date) {
+        return Arrays.stream(date.split("-"))
+                .map(string -> Integer.toBinaryString(Integer.parseInt(string)))
+                .collect(Collectors.joining("-"));
+    }
+}
