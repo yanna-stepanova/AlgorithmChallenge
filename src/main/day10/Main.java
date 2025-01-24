1afe17417322c1163878e49829cd83300142c907
Yana Stepanova <yanna.stepanova@gmail.com>
2025-01-24 17:14:55 +0200

    day10

---
+++ src/main/day10/Main.java
@@ -0,0 +1,8 @@
+package main.day10;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.validStrings(3));
+    }
+}
