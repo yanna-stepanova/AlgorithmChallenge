8f1a2eeec0cc0b429a69e49e471a8784a59d64e1
Yana Stepanova <yanna.stepanova@gmail.com>
2025-01-29 19:53:55 +0200

    day13

---
+++ src/main/day13/Solution.java
@@ -0,0 +1,19 @@
+package main.day13;
+
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int maxCounter = 0;
+        int rowIndex = 0;
+        for (int i = 0; i < mat.length; i++) {
+            int count = 0;
+            for (int number : mat[i]) {
+                count += number;
+            }
+            if (maxCounter < count) {
+                maxCounter = count;
+                rowIndex = i;
+            }
+        }
+        return new int[]{rowIndex, maxCounter};
+    }
+}
