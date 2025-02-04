d61fb70a8c3a5331909abcac873dbb560d2fd55c
Yana Stepanova <yanna.stepanova@gmail.com>
2025-02-04 23:16:56 +0200

    day17

---
+++ src/main/day17/Main.java
@@ -0,0 +1,8 @@
+package main.day17;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.makeSmallestPalindrome("egcfe"));
+    }
+}
