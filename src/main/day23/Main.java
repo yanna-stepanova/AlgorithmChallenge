73e3fe48846d1511dfcfe5d877bc6b48019d8cce
Yana Stepanova <yanna.stepanova@gmail.com>
2025-03-06 22:49:39 +0200

    tail

--- src/main/day23/Main.java
+++ src/main/day23/Main.java
@@ -2,7 +2,9 @@ package main.day23;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = new int[]{79,74,57,22}; //new int[]{5,4,3,2,1,1000000000};
+
         Solution solution = new Solution();
-        System.out.println();
+        System.out.println(solution.maxDistance(arr, 4));
     }
 }
