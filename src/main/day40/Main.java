37a5e9a4b7c0f5c9176bb355ba16fab220e1f2bf
Yana Stepanova <yanna.stepanova@gmail.com>
2025-03-07 19:48:16 +0200

    tail

---
+++ src/main/day40/Main.java
@@ -0,0 +1,8 @@
+package main.day40;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countTriplets(new int[]{2,3,1,6,7}));
+    }
+}
